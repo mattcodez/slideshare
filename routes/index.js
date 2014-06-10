6a37b7153bede1b2792d8af712aac1be9a39e2d7
Matthew Molnar <matthew.molnar@gmail.com>
2014-06-09 23:51:00 -0400

    Add route for main view

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,12 @@ module.exports = function(app) {
 	route.index = function (req, res) {
 	  res.render('index', {locals: { routes: app._router.stack }});
 	};
+	
+	// main.html
+	route.main = function(req, res){
+		res.render('main', {});
+	};
 
 	app.get('/', route.index);
+	app.get('/main', route.main);
 };
