eaf205e78906c72065ff435b6be98ca1a6323f27
Matthew Molnar <matthew.molnar@gmail.com>
2014-06-21 23:55:47 -0400

    Submit images via formData.

--- public/js/show.js
+++ public/js/show.js
@@ -7,6 +7,38 @@ function init(){
 	$.getJSON('/api/post/' + showId, function(show){
 		loadSection($(document.body), show.post);
 	});
+	
+	$('#newImage').submit(function(e){
+		e.preventDefault();
+		var form = $(this);
+		
+		var formData = getImageFormData(form.find('input[type=file]')[0].files);
+		
+		$.ajax({
+			url: '/api/post/' + showId,
+			data: formData,
+			processData: false,
+			contentType: 'multipart/form-data',
+			type: 'PUT',
+			success: function(data){
+			}
+		});
+	});
+}
+
+function getImageFormData(files){
+	var formData = new FormData();
+	for (var i = 0; i < files.length; i++) {
+		var file = files[i];
+
+		if (!file.type.match('image.*')) {
+			continue;
+		}
+
+		formData.append('photos', file, file.name);
+	}
+	
+	return formData;
 }
 
 function loadSection(el, data){
